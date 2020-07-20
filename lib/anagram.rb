e2a218d0bf7a41fb7970c5eea971a1046c5c9bf3
Juan Rivera <juanriv45@gmail.com>
2020-07-20 00:13:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,6 @@
 # Your code goes here!
 class Anagram
+<<<<<<< HEAD
   attr_accessor :name
   def initialize(word)
     @name = word
@@ -7,5 +8,22 @@ class Anagram
 
   def match(array)
     array.select { |test_word| name.split("").sort == test_word.split("").sort}
+=======
+attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+
+    array.select do |x|
+
+      if x.split("").sort == @word.split("").sort then
+        x
+      end
+    end
+
+
+>>>>>>> a3478ef25db95cf15f30e384b3859d74c06bfc6d
   end
 end
